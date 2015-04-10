40506ff00f32f90f3b93415fd012a821d37f7888
Fabrício Zimmerer Murta <fabricio@object.net>
2015-04-10 06:11:30 -0300

    Create the TicTacToe Bridge.NET project!

---
+++ Games/TicTacToe/BridgeTicTacToe/TicTacToe.cs
@@ -0,0 +1,365 @@
+﻿using Bridge;
+using Bridge.Html5;
+using Bridge.jQuery2;
+using System;
+
+namespace BridgeTicTacToe
+{
+    public class TicTacToe
+    {
+        #region Types
+        private enum Turn
+        {
+            Human,
+            Computer
+        }
+
+        private enum Player
+        {
+            Human,
+            Computer,
+            None
+        }
+        #endregion
+
+        #region Variables
+        private const string human = "x";
+        private const string computer = "o";
+
+        private static Turn turn = Turn.Human;
+
+        // Tic-Tac-Toe 3x3 board
+        private static string[,] board = new string[3, 3];
+
+        private static string[,] boardMap = new string[,]
+        {
+            { "a1", "a2", "a3" },
+            { "b1", "b2", "b3" },
+            { "c1", "c2", "c3" }
+        };
+
+        private static Player winner = Player.None;
+        #endregion
+
+        #region Game Logic
+
+        private static void GameFlow()
+        {
+            var hmn = BridgeTicTacToe.TicTacToe.human;
+
+            if (jQuery.This.Is("td") && turn == Turn.Human && string.IsNullOrWhiteSpace(jQuery.This.Text()))
+            {
+                jQuery.This.Text(hmn);
+                BridgeTicTacToe.TicTacToe.BoardCheck();
+                turn = Turn.Computer;
+
+                if (!CheckWin())
+                {
+                    BridgeTicTacToe.TicTacToe.CompMove();
+                    BridgeTicTacToe.TicTacToe.BoardCheck();
+                    BridgeTicTacToe.TicTacToe.CheckWin();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes the game flow.
+        /// </summary>
+        [Ready]
+        private static void PageReady()
+        {
+            jQuery.Select("#restart").Click(BridgeTicTacToe.TicTacToe.ClearBoard);
+            jQuery.Select("td").Click(BridgeTicTacToe.TicTacToe.GameFlow);
+
+            BridgeTicTacToe.TicTacToe.GameFlow();
+
+            jQuery.Select("#gameStatus").Css("background", "#bef");
+            jQuery.Select("#gameStatus").Text("Make your move!");
+
+            Console.Log("Upper bounds: " + board.GetUpperBound(0)); // FIXME: This returns NaN!?!?!?
+        }
+
+        /// <summary>
+        /// compMove's basic AI checks if there's a winning sequence, if there's a way to block
+        /// lose, or randomly picks a cell.
+        /// </summary>
+        private static void CompMove()
+        {
+            var cpu = BridgeTicTacToe.TicTacToe.computer;
+            var hmn = BridgeTicTacToe.TicTacToe.human;
+
+            var brd = BridgeTicTacToe.TicTacToe.board;
+            var bm = BridgeTicTacToe.TicTacToe.boardMap;
+
+            if (turn == Turn.Human)
+            {
+                return;
+            }
+
+            if (!FillGap(cpu) && !FillGap(hmn))
+            {
+                var freeCellList = new string[0];
+
+                for (var i = 0; i <= brd.GetUpperBound(0); i++)
+                {
+                    for (var j = 0; j <= brd.GetUpperBound(1); j++)
+                    {
+                        if (string.IsNullOrWhiteSpace(brd[i, j]))
+                        {
+                            freeCellList.Push(bm[i, j]);
+                        }
+                    }
+                }
+
+                if (freeCellList.Length > 0)
+                {
+                    var chosenCell = (int)Math.Floor(Math.Random() * freeCellList.Length);
+
+                    jQuery.Select("#" + freeCellList[chosenCell]).Text(cpu);
+                }
+            }
+
+            // Finished turn. Next turn will be human's.
+            turn = Turn.Human;
+        }
+
+        private static bool FillGap(string CheckChar)
+        {
+            var brd = BridgeTicTacToe.TicTacToe.board;
+            var bm = BridgeTicTacToe.TicTacToe.boardMap;
+
+            var myCheckMark = BridgeTicTacToe.TicTacToe.computer;
+
+            // [i,j] positions of component spaces to fill a row and win
+            int iCmp1;
+            int iCmp2;
+            int jCmp1;
+            int jCmp2;
+
+            string cmp1;
+            string cmp2;
+
+            var iBound = brd.GetUpperBound(0);
+            var jBound = brd.GetUpperBound(1);
+
+            var iCount = iBound + 1;
+            var jCount = jBound + 1;
+
+            for (var i = 0; i <= iBound; i++)
+            {
+                iCmp1 = (i + 1) % iCount;
+                iCmp2 = (i + 2) % iCount;
+                for (var j = 0; j <= jBound; j++)
+                {
+
+                    // If the inspected cell is empty
+                    if (string.IsNullOrWhiteSpace(brd[i, j]))
+                    {
+                        jCmp1 = (j + 1) % jCount;
+                        jCmp2 = (j + 2) % jCount;
+
+                        // Check if the column has two CheckChars already
+                        cmp1 = brd[iCmp1, j];
+                        cmp2 = brd[iCmp2, j];
+
+                        if (cmp1 == CheckChar && cmp2 == CheckChar)
+                        {
+                            jQuery.Select("#" + bm[i, j]).Text(myCheckMark);
+
+                            return true;
+                        }
+
+                        // Check if the line has two CheckChars already
+                        cmp1 = brd[i, jCmp1];
+                        cmp2 = brd[i, jCmp2];
+
+                        if (cmp1 == CheckChar && cmp2 == CheckChar)
+                        {
+                            jQuery.Select("#" + bm[i, j]).Text(myCheckMark);
+
+                            return true;
+                        }
+
+                        // If corners, check diagonals
+                        if ((i == 0 || i == iBound) && (j == 0 || j == jBound))
+                        {
+
+                            if (i == j)
+                            {
+                                // For i=j (0 or 2), we check cmps (1,1), (2,2) or (0,0), (1,1) resp.
+                                cmp1 = brd[iCmp1, jCmp1];
+                                cmp2 = brd[iCmp2, jCmp2];
+                            }
+                            else
+                            {
+                                // For i!=j (0,2 or 2,0) we check cmps (1,2), (2,1) or (0,1), (1,0)
+                                cmp1 = brd[iCmp1, jCmp2];
+                                cmp2 = brd[iCmp2, jCmp1];
+                            }
+
+                            if (cmp1 == CheckChar && cmp2 == CheckChar)
+                            {
+                                jQuery.Select("#" + bm[i, j]).Text(myCheckMark);
+
+                                return true;
+                            }
+                        }
+                        else
+                        {
+                            // If not corners and i=j (1,1, center), then check both diagonals
+                            if (i == j)
+                            {
+                                if ((brd[0, 0] == CheckChar && brd[2, 2] == CheckChar) ||
+                                    (brd[0, 2] == CheckChar && brd[2, 0] == CheckChar))
+                                {
+                                    jQuery.Select("#" + bm[i, j]).Text(myCheckMark);
+
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Tracks checked cells in the game
+        /// </summary>
+        private static void BoardCheck()
+        {
+            var brd = BridgeTicTacToe.TicTacToe.board;
+            var bm = BridgeTicTacToe.TicTacToe.boardMap;
+
+            for (var i = 0; i <= brd.GetUpperBound(0); i++)
+            {
+                for (var j = 0; j <= brd.GetUpperBound(1); j++)
+                {
+                    BridgeTicTacToe.TicTacToe.board[i, j] = jQuery.Select("#" + bm[i, j]).Text();
+                }
+            }
+        }
+
+        private static bool CheckWin()
+        {
+            var brd = BridgeTicTacToe.TicTacToe.board;
+
+            var rowMatch = new bool[]
+            {
+                brd[0, 0] == brd[0, 1] && brd[0, 1] == brd[0, 2],
+                brd[1, 0] == brd[1, 1] && brd[1, 1] == brd[1, 2],
+                brd[2, 0] == brd[2, 1] && brd[2, 1] == brd[2, 2]
+            };
+
+            var colMatch = new bool[]
+            {
+                brd[0, 0] == brd[1, 0] && brd[1, 0] == brd[2, 0],
+                brd[0, 1] == brd[1, 1] && brd[1, 1] == brd[2, 1],
+                brd[0, 2] == brd[1, 2] && brd[1, 2] == brd[2, 2]
+            };
+
+            var dgnMatch = new bool[] {
+                brd[0, 0] == brd[1, 1] && brd[1, 1] == brd[2, 2],
+                brd[0, 2] == brd[1, 1] && brd[1, 1] == brd[2, 0]
+            };
+
+            foreach (var mark in new string[] { human, computer })
+            {
+                if (
+                    BridgeTicTacToe.TicTacToe.winner == Player.None &&
+                    ((rowMatch[0] || colMatch[0]) && brd[0, 0] == mark) ||
+                    (rowMatch[1] && brd[1, 0] == mark) ||
+                    (rowMatch[2] && brd[2, 0] == mark) ||
+                    (colMatch[1] && brd[0, 1] == mark) ||
+                    (colMatch[2] && brd[0, 2] == mark) ||
+                    ((dgnMatch[0] || dgnMatch[1]) && brd[1, 1] == mark)
+                   )
+                {
+                    BridgeTicTacToe.TicTacToe.WinAlert(mark);
+                }
+            }
+
+            var draw = true;
+            for (var i = 0; i <= brd.GetUpperBound(0); i++)
+            {
+                for (var j = 0; j <= brd.GetUpperBound(1); j++)
+                {
+                    if (string.IsNullOrWhiteSpace(brd[i, j]))
+                    {
+                        draw = false;
+                        break;
+                    }
+                }
+
+                if (!draw)
+                {
+                    break;
+                }
+            }
+
+            if (draw)
+            {
+                jQuery.Select("#gameStatus").Css("background", "#eef");
+                jQuery.Select("#gameStatus").Text("Cat's game!");
+
+                return true;
+            }
+
+            return (winner != Player.None);
+        }
+
+        /// <summary>
+        /// Declares who won the match.
+        /// </summary>
+        private static void WinAlert(string who)
+        {
+            BridgeTicTacToe.TicTacToe.winner = (who == human) ? Player.Human : Player.Computer;
+
+            var whoWon = BridgeTicTacToe.TicTacToe.winner;
+
+            if (whoWon == Player.Human)
+            {
+                jQuery.Select("#gameStatus").Css("background", "#cfc");
+                jQuery.Select("#gameStatus").Text("Victory is yours!");
+            }
+            else if (whoWon == Player.Computer)
+            {
+                jQuery.Select("#gameStatus").Css("background", "#fbb");
+                jQuery.Select("#gameStatus").Text("Better luck next time!");
+            }
+        }
+
+        private static void ClearBoard()
+        {
+            var iBound = BridgeTicTacToe.TicTacToe.board.GetUpperBound(0);
+            var jBound = BridgeTicTacToe.TicTacToe.board.GetUpperBound(1);
+
+            var bm = BridgeTicTacToe.TicTacToe.boardMap;
+
+            for (var i = 0; i <= iBound; i++)
+            {
+                for (var j = 0; j <= jBound; j++)
+                {
+                    BridgeTicTacToe.TicTacToe.board[i, j] = String.Empty;
+
+                    jQuery.Select("#" + bm[i, j]).Text(String.Empty);
+                }
+            }
+
+            BridgeTicTacToe.TicTacToe.winner = Player.None;
+
+            jQuery.Select("#gameStatus").Css("background", "#bef");
+            jQuery.Select("#gameStatus").Text("Make your move!");
+
+            BridgeTicTacToe.TicTacToe.GameFlow();
+
+            if (turn == Turn.Computer)
+            {
+                BridgeTicTacToe.TicTacToe.CompMove();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Games/TicTacToe/TicTacWeb/js/originalTicTacToe.js
@@ -0,0 +1,210 @@
+﻿// VARIABLES
+
+var human = 'x'; // turn = 0
+var computer = 'o'; // turn = 1
+var compMove;
+var turn = 0; // toggles btw 0 and 1 for switching turns
+
+var boardCheck; // function to check value in each cell
+var a1; // value within each cell
+var a2;
+var a3;
+var b1;
+var b2;
+var b3;
+var c1;
+var c2;
+var c3;
+
+var cellList = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+
+var checkWin; // function that checks the board for winning combo
+var xWin = false; // true if X wins
+var oWin = false; // true if O wins
+var winAlert; // function that declares winner and restarts game
+
+var clearBoard;
+
+
+// Performs a step of the game flow. If click, then check clicked space if allowed.
+var gameFlow = function () {
+    if ($(this).is("td") && turn == 0 && $(this).text() == "") {
+        $(this).text(human);
+        boardCheck();
+        turn = 1;
+        if (!checkWin()) {
+            compMove();
+            boardCheck();
+            checkWin();
+        }
+    }
+};
+
+
+// Initializes the game flow.
+$(document).ready(function () {
+    $('#restart').click(clearBoard);
+    $('td').click(gameFlow);
+    gameFlow();
+
+    $('#gameStatus').css('background', '#bef');
+    $('#gameStatus').text('Make your move!');
+});
+
+// compMove's basic AI checks if there's a winning sequence, if there's a way to block lose, or randomly picks a cell.
+function compMove() {
+    if (turn == 0) return;
+
+    if (!fillGap(computer) && !fillGap(human)) {
+        // IF NO OPP TO BLOCK A WIN, THEN PLAY IN ONE OF THESE SQUARES
+        var freeCellList = [];
+        for (var i = 0; i < cellList.length; i++) {
+            if ($('#' + cellList[i]).text() == "") freeCellList.push(cellList[i]);
+        }
+
+        if (freeCellList.length > 0) {
+            var chosenCell = Math.floor(Math.random() * freeCellList.length);
+            $('#' + freeCellList[chosenCell]).text(computer);
+        }
+    }
+
+    // finished turn. next turn will be human's
+    turn = 0;
+};
+
+function fillGap(checkChar) {
+    if (a1 == "" && ((a3 == checkChar && a2 == checkChar) || (c3 == checkChar && b2 == checkChar) || (c1 == checkChar && b1 == checkChar))) {
+        $('#a1').text(computer);
+        return true;
+    }
+    else if (a2 == "" && ((a1 == checkChar && a3 == checkChar) || (c2 == checkChar && b2 == checkChar))) {
+        $('#a2').text(computer);
+        return true;
+    }
+    else if (a3 == "" && ((a1 == checkChar && a2 == checkChar) || (c1 == checkChar && b2 == checkChar) || (c3 == checkChar && b3 == checkChar))) {
+        $('#a3').text(computer);
+        return true;
+    }
+    else if (c3 == "" && ((c1 == checkChar && c2 == checkChar) || (a1 == checkChar && b2 == checkChar) || (a3 == checkChar && b3 == checkChar))) {
+        $('#c3').text(computer);
+        return true;
+    }
+    else if (c1 == "" && ((c3 == checkChar && c2 == checkChar) || (a3 == checkChar && b2 == checkChar) || (a1 == checkChar && b1 == checkChar))) {
+        $('#c1').text(computer);
+        return true;
+    }
+    else if (c2 == "" && ((c3 == checkChar && c1 == checkChar) || (a2 == checkChar && b2 == checkChar))) {
+        $('#c2').text(computer);
+        return true;
+    }
+    else if (b1 == "" && ((b3 == checkChar && b2 == checkChar) || (a1 == checkChar && c1 == checkChar))) {
+        $('#b1').text(computer);
+        return true;
+    }
+    else if (b3 == "" && ((a3 == checkChar && c3 == checkChar) || (b2 == checkChar && b1 == checkChar))) {
+        $('#b3').text(computer);
+        return true;
+    }
+    else if (b2 == "" && ((a3 == checkChar && c1 == checkChar) || (c3 == checkChar && a1 == checkChar) ||
+             (b3 == checkChar && b1 == checkChar) || (c2 == checkChar && a2 == checkChar))) {
+        $('#b2').text(computer);
+        return true;
+    }
+
+    return false;
+}
+
+// Tracks checked cells in the game
+function boardCheck() {
+    a1 = $('#a1').text();
+    a2 = $('#a2').text();
+    a3 = $('#a3').text();
+    b1 = $('#b1').text();
+    b2 = $('#b2').text();
+    b3 = $('#b3').text();
+    c1 = $('#c1').text();
+    c2 = $('#c2').text();
+    c3 = $('#c3').text();
+};
+
+// Checks against a player win, lose or tie
+checkWin = function () { // CHECKS IF X WON
+    if ((a1 == a2 && a1 == a3 && (a1 == "x")) || //first row
+    (b1 == b2 && b1 == b3 && (b1 == "x")) || //second row
+    (c1 == c2 && c1 == c3 && (c1 == "x")) || //third row
+    (a1 == b1 && a1 == c1 && (a1 == "x")) || //first column
+    (a2 == b2 && a2 == c2 && (a2 == "x")) || //second column
+    (a3 == b3 && a3 == c3 && (a3 == "x")) || //third column
+    (a1 == b2 && a1 == c3 && (a1 == "x")) || //diagonal 1
+    (a3 == b2 && a3 == c1 && (a3 == "x")) //diagonal 2
+    ) {
+        xWin = true;
+        winAlert();
+
+    } else { // CHECKS IF O WON
+        if ((a1 == a2 && a1 == a3 && (a1 == "o")) || //first row
+        (b1 == b2 && b1 == b3 && (b1 == "o")) || //second row
+        (c1 == c2 && c1 == c3 && (c1 == "o")) || //third row
+        (a1 == b1 && a1 == c1 && (a1 == "o")) || //first column
+        (a2 == b2 && a2 == c2 && (a2 == "o")) || //second column
+        (a3 == b3 && a3 == c3 && (a3 == "o")) || //third column
+        (a1 == b2 && a1 == c3 && (a1 == "o")) || //diagonal 1
+        (a3 == b2 && a3 == c1 && (a3 == "o")) //diagonal 2
+        ) {
+            oWin = true;
+            winAlert();
+
+        } else { // CHECKS FOR TIE GAME IF ALL CELLS ARE FILLED
+            if (((a1 == "x") || (a1 == "o")) && ((b1 == "x") || (b1 == "o")) && ((c1 == "x") || (c1 == "o")) && ((a2 == "x") || (a2 == "o")) && ((b2 == "x") || (b2 == "o")) && ((c2 == "x") || (c2 == "o")) && ((a3 == "x") || (a3 == "o")) && ((b3 == "x") || (b3 == "o")) && ((c3 == "x") || (c3 == "o"))) {
+                $('#gameStatus').css('background', '#eef');
+                $('#gameStatus').text("Cat's game!");
+                return true;
+            }
+        }
+    }
+    return (xWin || oWin);
+};
+
+
+// DECLARES WHO WON
+var winAlert = function () {
+    if (xWin == true) {
+        $('#gameStatus').css('background', '#cfc');
+        $('#gameStatus').text('Victory is yours!');
+    } else {
+        if (oWin == true) {
+            $('#gameStatus').css('background', '#fbb');
+            $('#gameStatus').text('Better luck next time!');
+        }
+    }
+};
+
+
+// NEWGAME BUTTON CLEARS THE BOARD, RESTARTS GAME, AND RESETS THE WINS
+var clearBoard = function (event) {
+    a1 = a2 = a3 = b1 = b2 = b3 = c1 = c2 = c3 = "";
+    $('#a1').text("");
+    $('#b1').text("");
+    $('#c1').text("");
+    $('#a2').text("");
+    $('#b2').text("");
+    $('#c2').text("");
+    $('#a3').text("");
+    $('#b3').text("");
+    $('#c3').text("");
+    xWin = false;
+    oWin = false;
+
+    $('#gameStatus').css('background', '#bef');
+    $('#gameStatus').text('Make your move!');
+
+    gameFlow();
+    if (turn == 1) compMove();
+};
+
+// STILL NEED TO FIX:
+// * Alert for tie game or xWin appears twice
+// * X's can replace O's
+// * Missed opportunities for O to win
+// * Almost never let's human win
+// * Clean up logic for compMove
