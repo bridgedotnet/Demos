9903045990dc775d2cf9a7656f8c6c80fc9062c7
Geoffrey McGill <geoff@object.net>
2015-04-30 04:24:16 -0600

    Improved .bat to xcopy required .dlls to /bin folder

--- VSCode/Demo/build/builder.bat
+++ VSCode/Demo/build/builder.bat
@@ -1,3 +1,5 @@
+start /wait xcopy build\*.dll bin\ /Y
+
 start /wait C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe ^
 	/target:library ^
 	/nostdlib+ ^
@@ -5,7 +7,5 @@ start /wait C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe ^
 	/r:Bridge.dll;Bridge.Html5.dll ^
 	/out:bin\Demo.dll ^
 	/recurse:*.cs
-
-:: xcopy build\Bridge.dll bin\Bridge.dll
-
+	
 start build\Bridge.Builder.exe -p Demo.csproj -b build\Bridge.dll -o Bridge\output\`
