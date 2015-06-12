468f8b8fc2d112351302d28181f02532857bb89e
wind39 <william.ivanski@gmail.com>
2015-06-12 10:04:13 -0300

    Demo project on MonoDevelop with intellisense

---
+++ MonoDevelop/BridgeDemoMD/App.cs
@@ -0,0 +1,15 @@
+﻿using Bridge;
+using Bridge.Html5;
+
+namespace Demo                                                                                                                                                                 
+{                                                                                                                                                                              
+    public class App                                                                                                                                                           
+    {                                                                                                                                                                          
+        [Ready]                                                                                                                                                                
+        public static void Main()                                                                                                                                              
+        {                                                                                                                                                                      
+            // Simple alert() to confirm it's working                                                                                                                          
+            Window.Alert("Success");
+        }                                                                                                                                                                      
+    }                                                                                                                                                                          
+}
