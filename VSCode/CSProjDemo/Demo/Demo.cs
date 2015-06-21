91e91e432fcb813db04d10ee39919ba5c3e8fe94
Fabrício Zimmerer Murta <fabricio@object.net>
2015-06-21 16:43:44 -0300

    Create using VS2013 a project that is meant to run on VSCode.

---
+++ VSCode/CSProjDemo/Demo/Demo.cs
@@ -0,0 +1,14 @@
+﻿using Bridge.Html5;
+
+namespace Demo
+{
+    public class DemoClass
+    {
+        [Ready]
+        public static void main()
+        {
+            Console.Log("Demo. Logged on console.");
+            Document.Body.AppendChild(new SpanElement() { InnerHTML = "Check results on the console log." });
+        }
+    }
+}
