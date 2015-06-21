d51756d22f7bc48e0e502a0f0b78a0f62923fef5
Fabrício Zimmerer Murta <fabricio@object.net>
2015-06-21 15:42:14 -0300

    Add project.json and review builder.sh script to also use NuGet packages.

--- VSCode/Demo/build/builder.sh
+++ VSCode/Demo/build/builder.sh
@@ -1,7 +1,28 @@
-mcs	/nostdlib \
-	/warn:0 \
-	/reference:"build/Bridge.dll;build/Bridge.Html5.dll" \
-	/out:build/Demo.dll \
-	/recurse:*.cs
+#!/bin/bash
 
+pkgbasedir="${HOME}/.dnx/packages/Bridge"
+lastver="$(ls "${pkgbasedir}" | sort -n | tail -n 1)"
+if [ -z "${lastver}" ]; then
+ echo "ERROR: Unable to locate Bridge package in local packages' directory."
+ echo "Did you run 'dnu restore'?"
+ exit 1
+fi
+
+pkgdir="/${pkgbasedir}/${lastver}"
+
+if [ ! -d "${pkgdir}" ]; then
+ echo "ERROR: Unable to locate Bridge package in local packages directory."
+ echo "Did you run 'dnu restore'?"
+ exit 1
+fi
+
+# Build using mono.
+mcs	-nostdlib \
+	-warn:0 \
+	-reference:"${pkgdir}/lib/net45/Bridge.dll;${pkgdir}/lib/net45/Bridge.Html5.dll" \
+	-out:build/Demo.dll \
+	-recurse:*.cs
+
+# Run Bridge builder to translate into JavaScript.
+#mono ${pkgdir}/Bridge.Builder.exe -lib build/Demo.dll
 mono build/Bridge.Builder.exe -lib build/Demo.dll
