a5dea4e303764a46e0e6b94579166d22b2e326c9
vladsch <vladimir@object.net>
2015-08-24 17:29:45 +0300

    [ADD] Class library template to compile a code for .NET and Bridge

---
+++ Template/SharedBridge/Class1.cs
@@ -0,0 +1,17 @@
+﻿#if BRIDGE
+using Bridge;
+using Bridge.Html5;
+#else
+using System;
+#endif
+
+namespace SharedBridge
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+    }
+}
