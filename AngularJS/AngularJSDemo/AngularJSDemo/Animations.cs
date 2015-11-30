101464e1bed594455a7806f5eda2f5c9be9f9a0a
Geoffrey McGill <geoff@object.net>
2015-11-30 08:47:48 -0700

    Add Animations.cs

---
+++ AngularJS/AngularJSDemo/AngularJSDemo/Animations.cs
@@ -0,0 +1,89 @@
+﻿using Bridge;
+using Bridge.AngularJS;
+using Bridge.AngularJS.Resource;
+using Bridge.AngularJS.Route;
+using Bridge.Html5;
+using Bridge.jQuery2;
+using System;
+
+namespace PhoneCat
+{
+    public partial class App
+    {
+        public static void InitAnimations()
+        {
+            var phonecatAnimations = Angular.Module("phonecatAnimations",
+                new string[] { "ngAnimate" });
+
+            phonecatAnimations.Animation(".phone", () =>
+            {
+                Func<jQuery, string, Action, Action<bool>> animateUp =
+                    (jQuery element, string className, Action done) =>
+                    {
+                        if (className != "active")
+                        {
+                            return null;
+                        }
+
+                        element.Css(
+                        new
+                        {
+                            Position = Position.Absolute,
+                            Top = 500,
+                            Left = 0,
+                            Display = Display.Block
+                        });
+
+                        element.Animate(new
+                        {
+                            Top = 0
+                        }, 400, "swing", done);
+
+                        return (cancel) =>
+                        {
+                            if (cancel)
+                            {
+                                element.Stop();
+                            };
+                        };
+                    };
+
+                Func<jQuery, string, Action, Action<bool>> animateDown =
+                    (jQuery element, string className, Action done) =>
+                    {
+                        if (className != "active")
+                        {
+                            return null;
+                        }
+
+                        element.Css(
+                        new
+                        {
+                            Position = Position.Absolute,
+                            Top = 0,
+                            Left = 0
+                        });
+
+                        element.Animate(new
+                        {
+                            Top = -500
+                        }, 400, "swing", done);
+
+                        return (cancel) =>
+                        {
+                            if (cancel)
+                            {
+                                element.Stop();
+                            };
+                        };
+                    };
+
+                return new Bridge.AngularJS.jQuery.Animation
+                {
+                    AddClass = animateUp,
+                    RemoveClass = animateDown
+                };
+            });
+        }
+    }
+}
--- AngularJS/AngularJSDemo/AngularJSDemo/App.cs
+++ AngularJS/AngularJSDemo/AngularJSDemo/App.cs
@@ -8,7 +8,7 @@ using System;
 
 namespace PhoneCat
 {
-    public class App
+    public partial class App
     {
         public App() { }
 
@@ -63,82 +63,6 @@ namespace PhoneCat
 
             services.Factory<Func<Func<string, object, ResourceActions,
                 Resource>, Resource>>("phoneService", PhoneServicesFactoryFn);
-        }
-
-        public static void InitAnimations()
-        {
-            var animations = Angular.Module("phonecatAnimations",
-                new string[] { "ngAnimate" });
-
-            animations.Animation(".phone", () =>
-            {
-                Func<jQuery, string, Action, Action<bool>> animateUp =
-                    (jQuery element, string className, Action done) =>
-                    {
-                        if (className != "active")
-                        {
-                            return null;
-                        }
-
-                        element.Css(
-                        new
-                        {
-                            Position = Position.Absolute,
-                            Top = 500,
-                            Left = 0,
-                            Display = Display.Block
-                        });
-
-                        element.Animate(new
-                        {
-                            Top = 0
-                        }, 400, "swing", done);
-
-                        return (cancel) =>
-                        {
-                            if (cancel)
-                            {
-                                element.Stop();
-                            };
-                        };
-                    };
-
-                Func<jQuery, string, Action, Action<bool>> animateDown =
-                    (jQuery element, string className, Action done) =>
-                    {
-                        if (className != "active")
-                        {
-                            return null;
-                        }
-
-                        element.Css(
-                        new
-                        {
-                            Position = Position.Absolute,
-                            Top = 0,
-                            Left = 0
-                        });
-
-                        element.Animate(new
-                        {
-                            Top = -500
-                        }, 400, "swing", done);
-
-                        return (cancel) =>
-                        {
-                            if (cancel)
-                            {
-                                element.Stop();
-                            };
-                        };
-                    };
-
-                return new Bridge.AngularJS.jQuery.Animation
-                {
-                    AddClass = animateUp,
-                    RemoveClass = animateDown
-                };
-            });
         }
 
         public static void RouteProviderFn([Name("$routeProvider")] RouteProvider provider)
