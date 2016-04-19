b58c7fd30ef2b9ff66637ba1aff807172b5d20a3
Geoffrey McGill <geoff@object.net>
2016-04-19 16:07:54 -0600

    Add some comments to App.cs

--- VSCode/Demo/App.cs
+++ VSCode/Demo/App.cs
@@ -8,16 +8,26 @@ namespace Demo
         [Ready]
         public static void Main()
         {
+            // Create a new Button
             var button = new ButtonElement
             {
                 InnerHTML = "Submit",
                 OnClick = (ev) =>
                 {
-                    Global.Alert("Welcome to Bridge.NET");
+                    // When Button is clicked, popup a message
+                    Global.Alert("Success!");
                 }
             };
 
+            // Add the Button to the page
             Document.Body.AppendChild(button);
+
+            // To confirm Bridge.NET is working: 
+            // 1. Build this project (Ctrl + Shift + B)
+            // 2. Browse to file /Bridge/www/demo.html
+            // 3. Right-click on file and select "View in Browser" (Ctrl + Shift + W)
+            // 4. File should open in a browser, click the "Submit" button
+            // 5. Success!
         }
     }
 }
