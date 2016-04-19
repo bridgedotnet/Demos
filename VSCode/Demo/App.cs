f07988dde4d649fbb4403601c60b7362af65e117
Fabrício Zimmerer Murta <fabricio@object.net>
2016-04-19 18:30:41 -0300

    Update VSCode demo's 'Demo.cs' into 'App.cs'.

---
+++ VSCode/Demo/App.cs
@@ -0,0 +1,23 @@
+using Bridge;
+using Bridge.Html5;
+
+namespace Demo
+{
+    public class App
+    {
+        [Ready]
+        public static void Main()
+        {
+            var button = new ButtonElement
+            {
+                InnerHTML = "Submit",
+                OnClick = (ev) =>
+                {
+                    Global.Alert("Welcome to Bridge.NET");
+                }
+            };
+
+            Document.Body.AppendChild(button);
+        }
+    }
+}
