546f5364b466ba335afef4a46f2f25058746db7f
Fabrício Zimmerer Murta <fabricio@object.net>
2016-04-19 18:42:25 -0300

    Update paths on VSCode demo's build scripts.

--- VSCode/Demo/.build/builder.sh
+++ VSCode/Demo/.build/builder.sh
@@ -1,7 +1,7 @@
 mcs	/nostdlib \
 	/warn:0 \
-	/reference:"build/Bridge.dll;build/Bridge.Html5.dll" \
-	/out:build/Demo.dll \
+	/reference:".build/Bridge.dll;.build/Bridge.Html5.dll" \
+	/out:.build/Demo.dll \
 	/recurse:*.cs
 
-mono build/Bridge.Builder.exe -lib build/Demo.dll
+mono .build/Bridge.Builder.exe -lib .build/Demo.dll
