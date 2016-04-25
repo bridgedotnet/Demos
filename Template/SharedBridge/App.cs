60f5480ea1f199aeaa25c86c340fb95fdb71a923
DaniilVeriga <daniil@object.net>
2016-04-25 16:38:24 +0300

    [UPDATE] Updated the SharedBridge template to Bridge 1.12.1.

---
+++ Template/SharedBridge/App.cs
@@ -0,0 +1,21 @@
+﻿#if BRIDGE
+using Bridge;
+using Bridge.Html5;
+#else
+using System;
+#endif
+
+namespace SharedBridge
+{
+    public class App
+    {
+
+        #if BRIDGE
+        [Ready]
+        #endif
+        public static void Main()
+        {
+            
+        }
+    }
+}
