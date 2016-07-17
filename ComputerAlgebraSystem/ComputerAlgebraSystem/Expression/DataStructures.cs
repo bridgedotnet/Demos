25e693400d59b436e07d8813fa7bb72c88fce214
Zaid-Ajaj <zaid.naom@gmail.com>
2016-07-18 00:21:02 +0200

    Computer algebra system demo with Bridge.NET

---
+++ ComputerAlgebraSystem/ComputerAlgebraSystem/Expression/DataStructures.cs
@@ -0,0 +1,68 @@
+﻿namespace ComputerAlgebraSystem
+{
+    public interface IExpr
+    {
+    }
+
+    public class Constant : IExpr
+    {
+
+        public Constant(double value)
+        {
+            this.Value = value;
+        }
+
+        public double Value { get; }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    public class Symbol : IExpr
+    {
+        public string Value { get; }
+
+        public Symbol(string symbol)
+        {
+            this.Value = symbol;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    public class Function : IExpr
+    {
+        public IExpr FirstArgument { get; }
+        public IExpr SecondArgument { get; }
+        public bool IsBinary { get; }
+        public string Name { get; }
+
+        public Function(string name, IExpr fstExpr)
+        {
+            this.IsBinary = false;
+            this.Name = name;
+            this.FirstArgument = fstExpr;
+        }
+
+        public Function(string name, IExpr fst, IExpr snd)
+        {
+            this.IsBinary = true;
+            this.Name = name;
+            this.FirstArgument = fst;
+            this.SecondArgument = snd;
+        }
+
+        public override string ToString()
+        {
+            return IsBinary ?
+                string.Format("{0}({1}, {2})", Name, FirstArgument, SecondArgument)
+              : string.Format("{0}({1})", Name, FirstArgument);
+        }
+
+    }
+}
---
+++ ComputerAlgebraSystem/ComputerAlgebraSystem/TinyPlotter/TinyPlotter.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using Bridge.Html5;
+
+namespace ComputerAlgebraSystem
+{
+    public class TinyPlotter
+    {
+        public PlotSettings Settings;
+        public HTMLCanvasElement Canvas;
+
+        public TinyPlotter(PlotSettings settings)
+        {
+            Settings = settings;
+            Canvas = new HTMLCanvasElement();
+            Canvas.Height = Settings.Height;
+            Canvas.Width = Settings.Width;
+            Canvas.Style.Border = "1px solid black";
+            var ctx = Canvas.GetContext(CanvasTypes.CanvasContext2DType.CanvasRenderingContext2D);
+            var image = ctx.CreateImageData(Canvas.Width, Canvas.Height);
+            if (Settings.DrawXAxis)
+                DrawXAxis(image);
+            if (Settings.DrawXAxis)
+                DrawYAxis(image);
+            ctx.PutImageData(image, 0, 0);
+        }
+
+        public void Draw()
+        {
+            var ctx = Canvas.GetContext(CanvasTypes.CanvasContext2DType.CanvasRenderingContext2D);
+            var image = ctx.CreateImageData(Canvas.Width, Canvas.Height);
+
+            if (Settings.DrawXAxis)
+                DrawXAxis(image);
+
+            if (Settings.DrawXAxis)
+                DrawYAxis(image);
+
+            foreach (var curve in Settings.Curves)
+                DrawCurve(curve, image);
+
+            ctx.PutImageData(image, 0, 0);
+        }
+
+        void DrawXAxis(ImageData image)
+        {
+            var xmin = Settings.Viewport.XMin;
+            var xmax = Settings.Viewport.XMax;
+            var ymin = Settings.Viewport.YMin;
+            var ymax = Settings.Viewport.YMax;
+            var step = Settings.StepX;
+
+            for (double x = xmin; x <= xmax; x += 0.01)
+            {
+                var point = new Point { X = x, Y = 0 };
+                var pointFromPlain = FromPointOnPlain(point, xmin, xmax, ymin, ymax, Canvas.Height, Canvas.Width);
+                SetPixel(image, (int)pointFromPlain.X, (int)pointFromPlain.Y, new Color { Red = 0, Blue = 0, Green = 0 });
+            }
+        }
+
+        void DrawYAxis(ImageData image)
+        {
+            var xmin = Settings.Viewport.XMin;
+            var xmax = Settings.Viewport.XMax;
+            var ymin = Settings.Viewport.YMin;
+            var ymax = Settings.Viewport.YMax;
+            var step = Settings.StepX;
+
+            for (double y = ymin; y <= ymax; y += 0.01)
+            {
+                var point = new Point { X = 0, Y = y };
+                var pointFromPlain = FromPointOnPlain(point, xmin, xmax, ymin, ymax, Canvas.Height, Canvas.Width);
+                SetPixel(image, (int)pointFromPlain.X, (int)pointFromPlain.Y, new Color { Red = 0, Blue = 0, Green = 0 });
+            }
+        }
+        void DrawCurve(Curve curve, ImageData image)
+        {
+            var xmin = Settings.Viewport.XMin;
+            var xmax = Settings.Viewport.XMax;
+            var ymin = Settings.Viewport.YMin;
+            var ymax = Settings.Viewport.YMax;
+            var step = Settings.StepX;
+
+            for (double x = xmin; x <= xmax; x += step)
+            {
+                var y = curve.Map(x);
+                if (y < ymin || y > ymax || double.IsNaN(y)) // off bounds or undefined
+                    continue;
+
+                var point = new Point { X = x, Y = y };
+                var pointFromPlain = FromPointOnPlain(point, xmin, xmax, ymin, ymax, Canvas.Height, Canvas.Width);
+                SetPixel(image, (int)pointFromPlain.X, (int)pointFromPlain.Y, curve.Color);
+            }
+        }
+
+
+        double Rescale(double value, Interval realRange, Interval projection)
+        {
+            if (value > realRange.To || value < realRange.From)
+            {
+                throw new ArgumentException("value is not the real range");
+            }
+                
+            var percentageOfProjection = (Math.Abs(projection.To - projection.From) * Math.Abs(value - realRange.From)) / Math.Abs(realRange.To - realRange.From);
+
+            return percentageOfProjection + projection.From;
+        }
+
+
+        Point FromPointOnPlain(Point p, double xmin, double xmax, double ymin, double ymax, double height, double width)
+        {
+            return new Point
+            {
+                X = Rescale(p.X,
+                         new Interval { From = xmin, To = xmax },
+                         new Interval { From = 0, To = width }),
+                       
+
+                Y = height - Rescale(p.Y,
+                                new Interval { From = ymin, To = ymax },
+                                new Interval { From = 0, To = height }),
+            };
+        }
+
+        void SetPixel(ImageData img, int x, int y, Color color)
+        {
+            int index = (x + y * (int)img.Width) * 4;
+            img.Data[index] = color.Red;
+            img.Data[index + 1] = color.Green;
+            img.Data[index + 2] = color.Blue;
+            img.Data[index + 3] = 255; // alpha
+        }
+    }
+}
