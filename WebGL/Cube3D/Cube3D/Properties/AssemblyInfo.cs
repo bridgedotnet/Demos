e3a7f5ea4d60de49e1aca72d506cd80c7c57ea2f
LeonidVeriga <leonid@object.net>
2017-07-20 21:24:16 +0300

    Update WebGL Cube3D to Bridge 16.0.0-beta5

--- WebGL/Cube3D/Cube3D/Properties/AssemblyInfo.cs
+++ WebGL/Cube3D/Cube3D/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Bridge;
 
 [assembly: AssemblyTitle("Cube3D")]
 [assembly: AssemblyDescription("Bridge WebGL 3D Cube Demo")]
@@ -13,3 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("87fe353d-8cbc-4aa2-a56b-7cb4c0aa41d3")]
 [assembly: AssemblyVersion("1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Convention(Member = ConventionMember.Field | ConventionMember.Method, Notation = Notation.LowerCamelCase)]
+[assembly: Convention(Target = ConventionTarget.ObjectLiteral, Member = ConventionMember.Property, Notation = Notation.LowerCamelCase)]
