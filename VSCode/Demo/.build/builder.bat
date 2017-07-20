a414d02dae6947e5d007aa62fc4c75e81e5ecb85
LeonidVeriga <leonid@object.net>
2017-07-21 02:03:17 +0300

    Update VSCode_Demo to Bridge 16.0.0-beta5

--- VSCode/Demo/.build/builder.bat
+++ VSCode/Demo/.build/builder.bat
@@ -5,4 +5,4 @@ start /wait C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe ^
 	/out:.build\Demo.dll ^
 	/recurse:*.cs
 	
-start /b .build\Bridge.Builder.exe -lib .build\Demo.dll
+start /b .build\bridge.exe -lib .build\Demo.dll
