e68db016e0d6477a8a1fad47414d50e5c3bd4821
LeonidVeriga <leonid@object.net>
2017-07-21 01:28:37 +0300

    Update WeatherApp to Bridge 16.0.0-beta5

--- Mobile/Weather/WeatherApp/App.cs
+++ Mobile/Weather/WeatherApp/App.cs
@@ -9,16 +9,16 @@ namespace WeatherApp
     public class App
     {
         /*
-         * WU DEVELOPER KEY TERMS OF USE 
-         * Weather Underground, hereafter "WU", (http://api.wunderground.com/api/) provides weather data via its web API. 
-         * A developer key is required as a parameter to API calls. By default, Bridge.NET, Inc., hereafter "The company", 
-         * provides you with a hardcoded WU developer key. The company authorizes you to use this key solely for the purposes 
-         * of building and running this Demo project as is. 
-         * 
+         * WU DEVELOPER KEY TERMS OF USE
+         * Weather Underground, hereafter "WU", (http://api.wunderground.com/api/) provides weather data via its web API.
+         * A developer key is required as a parameter to API calls. By default, Bridge.NET, Inc., hereafter "The company",
+         * provides you with a hardcoded WU developer key. The company authorizes you to use this key solely for the purposes
+         * of building and running this Demo project as is.
+         *
          * *Any other use is strictly prohibited*.
-         * 
-         * If you want to use the Bridge Weather Demo App in any other context or for any other purpose, you need to 
-         * obtain your own WU developer key. You can sign up for a WU developer key at the following address, 
+         *
+         * If you want to use the Bridge Weather Demo App in any other context or for any other purpose, you need to
+         * obtain your own WU developer key. You can sign up for a WU developer key at the following address,
          * for free: http://api.wunderground.com/weather/api/d/login.html?MR=1
          */
 
@@ -27,11 +27,11 @@ namespace WeatherApp
 
         [Ready]
         public static void Main()
-        {           
+        {
             GetForecast();
 
             // refresh button
-            new jQuery(new ButtonElement
+            new jQuery(new HTMLButtonElement
             {
                 Type = ButtonType.Button,
                 ClassName = "btn btn-primary",
@@ -71,7 +71,7 @@ namespace WeatherApp
 
                     jQuery.Select("#img").Attr("src", img.ToString());
 
-                    // select the user prefered metric                    
+                    // select the user prefered metric
                     jQuery.Select("#" + metric)
                         .Attr("checked", "checked")
                         .Parent().AddClass("active");
