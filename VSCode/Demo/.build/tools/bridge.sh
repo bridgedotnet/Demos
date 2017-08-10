3aa5592ed45efb87695c02f09669f78760f20a7e
Geoffrey McGill <geoff@object.net>
2017-08-10 15:37:44 -0600

    Use libs folder

--- VSCode/Demo/.build/tools/bridge.sh
+++ VSCode/Demo/.build/tools/bridge.sh
@@ -1,7 +1,7 @@
 mcs	/nostdlib \
 	/warn:0 \
 	/reference:".build/libs/Bridge.dll;.build/libs/Bridge.Html5.dll" \
-	/out:.build/Demo.dll \
+	/out:.build/libs/Demo.dll \
 	/recurse:*.cs
 
-mono .build/tools/bridge.exe -b .build/libs/Bridge.dll -lib .build/Demo.dll
+mono .build/tools/bridge.exe -b .build/libs/Bridge.dll -lib .build/libs/Demo.dll
