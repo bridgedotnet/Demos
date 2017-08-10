ad062b5b79f9351773d2e8f0e9146a5038a396e9
Geoffrey McGill <geoff@object.net>
2017-08-10 15:18:22 -0600

    Add tools .dll's

--- VSCode/Demo/.build/tools/bridge.bat
+++ VSCode/Demo/.build/tools/bridge.bat
@@ -2,7 +2,7 @@ start /wait C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe ^
 	/nostdlib ^
 	/warn:0 ^
 	/reference:.build\libs\Bridge.dll;.build\libs\Bridge.Html5.dll ^
-	/out:.build\Demo.dll ^
+	/out:.build\libs\Demo.dll ^
 	/recurse:*.cs
 	
-start .build\tools\bridge.exe -b .build\libs\Bridge.dll -lib .build\Demo.dll
+start .build\tools\bridge.exe -b .build\libs\Bridge.dll -lib .build\libs\Demo.dll
