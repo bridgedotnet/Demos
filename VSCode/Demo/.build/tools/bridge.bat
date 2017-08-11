29072e2c47a5a64ba844e5d1af7fc7752880297a
Geoffrey McGill <geoff@object.net>
2017-08-10 23:40:53 -0600

    Sync

--- VSCode/Demo/.build/tools/bridge.bat
+++ VSCode/Demo/.build/tools/bridge.bat
@@ -1,8 +1,7 @@
-start /wait .build\tools\csc.exe ^
+START /wait .build\tools\csc.exe ^
 	/nostdlib ^
 	/warn:0 ^
 	/reference:.build\lib\Bridge.dll;.build\lib\Bridge.Html5.dll;.build\lib\Newtonsoft.Json.dll; ^
-	/out:.build\lib\Demo.dll ^
 	/recurse:*.cs
-	
-start .build\tools\bridge.exe -b .build\lib\Bridge.dll -lib .build\lib\Demo.dll
+
+START .build\tools\bridge.exe -b .build\lib\Bridge.dll -lib .build\lib\Demo.dll
